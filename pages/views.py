deeaac83d763381cdc3f74238d5cb49226c2fe3a
chienchuanw <chienchuanwww@gmail.com>
2024-11-27 13:39:44 +0800

    modify: rename index to home

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "pages/index.html")
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, "pages/home.html")
