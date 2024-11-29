7d9701a2846de699d5f86dca1a7b5f1ad85db24f
chienchuanw <chienchuanwww@gmail.com>
2024-11-29 16:40:54 +0800

    feat: add create (add) functionality

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
-
-# Create your views here.
+from tasks.models import Task
+from django.contrib import messages
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "tasks/index.html")
+
+
+def add(request: HttpRequest) -> HttpResponse:
+    if request.POST:
+        task = Task()
+        task.title = request.POST.get("title")
+        task.description = request.POST.get("description")
+        task.save()
+
+        messages.success(request, "創建成功")
+        return redirect("tasks:add")
+
+    return render(request, "tasks/add.html")
